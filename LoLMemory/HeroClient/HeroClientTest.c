3c2019287a547d3b28f881a0df520c196991f86e
Spl3en <spl3en.contact@gmail.com>
2014-10-16 22:42:01 +0200

    TODO : Hero Client test unit.

---
+++ LoLMemory/HeroClient/HeroClientTest.c
@@ -0,0 +1,18 @@
+#include "HeroClient.h"
+#include "LoLClientAPI.h"
+
+#define __DEBUG_OBJECT__ "HeroClientTest"
+#include "dbg/dbg.h"
+
+
+/*
+ * Description : Unit tests checking if a HeroClient is coherent
+ * HeroClient *this : The instance to test
+ * Return : true on success, false on failure
+ */
+bool
+HeroClient_test (
+	HeroClient *this
+) {
+	return true;
+}
