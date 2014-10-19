0c220c315e66be622a1bf1683b47fb825b505871
Spl3en <spl3en.contact@gmail.com>
2014-10-19 04:18:50 +0200

    Improve Python wrapper API.

--- PythonWrapper/LoLClientAPI.py
+++ PythonWrapper/LoLClientAPI.py
@@ -44,6 +44,7 @@ class LoLClientAPI:
 	# ================================== Cursor APIs ==================================
 	# =================================================================================
 
+	
 	# Get the cursor camera position
 	# Returns : {x, y}
 	def get_cursor_position (self):
@@ -74,8 +75,74 @@ class LoLClientAPI:
 		self.hAPI.get_destination_position (byref(x), byref(y));
 		return (x.value, y.value)
 
+		
+	# =================================================================================
+	# ================================ Champions APIs =================================
+	# =================================================================================
+
+	
+	# Get the position of the current champion
+	# Returns : {x, y}
+	def get_champion_position (self):
+	
+		# C API declaration :
+		# void get_champion_position (
+		#	__out__ float * x,
+		#	__out__ float * y
+		# )
+
+		x = c_float()
+		y = c_float()
+		self.hAPI.get_champion_position (byref(x), byref(y));
+		return (x.value, y.value)
+
+	# Get the position of the current champion
+	# Returns : {currentHP, maximumHP}
+	def get_champion_hp (self):
+	
+		# C API declaration :
+		# void get_champion_hp (
+		#	__out__ float * currentHP,
+		#	__out__ float * maximumHP
+		# )
+
+		currentHP = c_float()
+		maximumHP = c_float()
+		self.hAPI.get_champion_hp (byref(currentHP), byref(maximumHP));
+		return (currentHP.value, maximumHP.value)
+
+	# Get the position of the current champion
+	# Returns : 0 if BLUE team, 1 if PURPLE team 
+	def get_champion_team (self):
+	
+		# C API declaration :
+		# int get_champion_team (
+		#	void
+		# )
+
+		return self.hAPI.get_champion_team ();
+
+		
+	# =================================================================================
+	# ================================= Summoner APIs =================================
+	# =================================================================================
+
+	# Get the name of the current summoner playing
+	# Returns : The name of the current summoner as an ASCII string
+	def get_current_summoner_name (self):
+	
+		# C API declaration :
+		# char * get_current_summoner_name (
+		#	void
+		# )
+
+		return self.hAPI.get_current_summoner_name ();
 
 	def __init__ (self):
 		# Load LoLClientAPI into LoL process
-		self.hAPI = cdll.LoadLibrary ('./LoLClientAPI.dll');
+		try:
+			self.hAPI = cdll.LoadLibrary ('./LoLClientAPI.dll');
+		except:
+			print "Cannot find LoLClientAPI.dll.";
+			exit(0);
 		
