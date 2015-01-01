06a59acd5763f4f9df4276a8701b69ce7c7c4fc5
Spl3en <spl3en.contact@gmail.com>
2015-01-01 05:10:49 +0100

    Add a basic exemple script for controlling chat APIs.

--- PythonWrapper/ChatController.py
+++ PythonWrapper/ChatController.py
@@ -3,13 +3,17 @@ import time
 
 api = LoLClientAPI.LoLClientAPI ();
 
+api.log_chat_message ('<font color="#00ff00">LoLClientAPI initialization ...</font>');
+
+api.log_chat_message ('<font color="#ff0000">LoLClientAPI : Red message</font>');
+api.log_chat_message ('<font color="#00ff00">LoLClientAPI : Green message</font>');
+api.log_chat_message ('<font color="#0000ff">LoLClientAPI : Blue message</font>');
+api.log_chat_message ('<font color="#000000">LoLClientAPI : Black message</font>');
+api.log_chat_message ('<font color="#ffffff">LoLClientAPI : White message</font>');
+
 while True:
-	print "Sending ..."
-	api.send_chat_message ("Hello");
-	
-	lastMsg = api.get_chat_message ();
-	if lastMsg is not None:
-		print "> %s" % lastMsg;
-	
-	
+	message = api.get_chat_message ();
+	if message not None:
+		print "Message received > %s" % message;
+		
 	time.sleep (1);
